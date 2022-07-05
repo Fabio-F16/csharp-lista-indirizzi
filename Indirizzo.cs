836c460bce939dd285e45e3345cd9a6ca8fb3f4a
Fabio-F16 <fipsfabio95@gmail.com>
2022-07-05 15:07:30 +0200

    parziale

---
+++ Indirizzo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    internal class Indirizzo
+    {
+        string Nome { get; set; }
+        string Cognome { get; set; }
+        string Via { get; set; }
+        string Citta { get; set; }
+        string Provincia { get; set; }
+        string Cap { get; set; }
+
+        public Indirizzo(string nome, string cognome, string via, string citta, string provincia, string cap)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Via = via;
+            Citta = citta;
+            Provincia = provincia;
+            Cap = cap;
+        }
+
+        public string Stampa()
+        {
+            string stringa = "******indirizzo******\n";
+            stringa += "Nome:\t" + this.Nome + "\n";
+            stringa += "Cognome:\t" + this.Cognome + "\n";
+            stringa += "Via:\t" + this.Via + "\n";
+            stringa += "Città:\t" + this.Citta + "\n";
+            stringa += "Provincia:\t" + this.Provincia + "\n";
+            stringa += "Cap:\t" + this.Cap + "\n";
+
+            return stringa;
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          //  Console.WriteLine("Hello, World!");
+
+            StreamReader indirizzi = File.OpenText("C:\\Users\\Fabioz\\Desktop\\Net\\csharp-lista-indirizzi\\addresses.csv");
+
+            string intestazione = indirizzi.ReadLine();
+            Console.WriteLine(intestazione);
+
+            List<Indirizzo> listaIndirizzi = new List<Indirizzo>();
+            List<string> listaIndirizziSbagliati = new List<string>();
+
+
+            while (!indirizzi.EndOfStream)
+            {
+                string linea = indirizzi.ReadLine();
+                //Console.WriteLine(linea);
+                string[] data = linea.Split(",");
+
+                for(int i = 0; i < data.Length; i++)
+                {
+                    Console.WriteLine(data[i]);
+                }
+
+                //try
+                //{
+                  
+                //    string nome = data[0];
+                //    string cognome = data[1];
+                //    string via = data[2];
+                //    string città = data[3];
+                //    string provincia = data[4];
+                //    string cap = data[5];
+
+                //    Indirizzo indirizzo = new Indirizzo(nome, cognome, via, città, provincia, cap);
+                //    listaIndirizzi.Add(indirizzo);
+                //    indirizzo.Stampa();
+                //}
+                //catch(IndexOutOfRangeException e)
+                //{
+                //    listaIndirizziSbagliati.Add(linea);
+                //}               
+            }
+            indirizzi.Close();
         }
     }
 }
